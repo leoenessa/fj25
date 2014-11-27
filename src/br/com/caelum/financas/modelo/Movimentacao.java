3cca81936417ffe67f42fa3876bb1659f9ecfb48
Leonardo Oliveira <leonardooc@globo.com>
2014-11-27 15:50:45 -0200

    pag 51

--- src/br/com/caelum/financas/modelo/Movimentacao.java
+++ src/br/com/caelum/financas/modelo/Movimentacao.java
@@ -4,14 +4,74 @@ import java.math.BigDecimal;
 import java.util.Calendar;
 
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.hibernate.annotations.ManyToAny;
 
 @Entity
 public class Movimentacao {
 	
+	@Id
+	@GeneratedValue
 	private Integer Id;
 	private String descricao;
+	
+	@Temporal(TemporalType.TIMESTAMP)
 	private Calendar data;
 	private BigDecimal valor;
+	
+	@ManyToOne
 	private Conta conta;
+	
+	@Enumerated(EnumType.STRING)
+	private TipoMovimentacao tipoMovimentacao;
+	
+	public Integer getId() {
+		return Id;
+	}
+	
+	public void setId(Integer id) {
+		Id = id;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public Calendar getData() {
+		return data;
+	}
+	
+	public void setData(Calendar data) {
+		this.data = data;
+	}
+	
+	public BigDecimal getValor() {
+		return valor;
+	}
+	
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+	
+	public Conta getConta() {
+		return conta;
+	}
+	
+	public void setConta(Conta conta) {
+		this.conta = conta;
+	}
+	
+	
 	
 }
